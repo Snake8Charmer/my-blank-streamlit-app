704e2a4f86ba1a946f0c878102e8278261ebec99
Wilson <paul.wilson@cgi.com>
2026-05-14 14:32:35 -0400

    Update app and project config

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My First Blank app")
-st.write("Hello, World!")
+st.write("Hello, Big World!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
