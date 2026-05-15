66dec816a961ad80ef295997ce35220bccd8e37d
Paul Wilson <paul.wilson@cgi.com>
2026-05-15 09:26:36 -0400

    Added "Blue"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My First Blank app")
-st.write("Hello, Big World!")
+st.write("Hello, Big Blue World!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
